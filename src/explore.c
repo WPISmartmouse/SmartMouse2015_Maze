7faa3ef496600ba9fa64c706db2bba739122b968
peter mitrano <mitranopeter@gmail.com>
2015-03-29 16:22:58 -0400

    updated docs

--- src/explore.c
+++ src/explore.c
@@ -3,14 +3,26 @@
  * \section intro_sec Introduction
  *
  * All function documentation is done in the header files. To see notes for each function, click on the header file it belongs to.
-
+ *
  * If there are any issues/bugs, use the issue tracker on the github repository. 
  * 	[Issue tracker] (https://github.com/WPISmartmouse/SmartMouse2015_Maze/issues)
+ *
+ * \section run_sec_linux Building & Running (Linux)
+ * 
+ * to make, run
+ *      
+ *      make;./bin/explore
+ * 
+ * you can make more debug information print out by making with the ARGS argument
+ * 
+ *      make ARGS=-DDEBUG_PATH #this will show the mouse in the maze as it goes. press enter to step forward
+ *      make ARGS=-DDEBUG #minimal debug info
+ *      make ARGS=-DDBEUG_FULL #a whole ton of debug information
  *
  * \section install_sec_linux Installation (Linux)
  *
  * \subsection step1_linux Clone the repository
-
+ *
  *      git clone https://github.com/WPISmartmouse/SmartMouse2015_Maze/
  *
  * \section install_sec_mac Installation (Mac)
